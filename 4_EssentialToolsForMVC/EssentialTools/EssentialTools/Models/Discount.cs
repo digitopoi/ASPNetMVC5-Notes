b6520e545de04f89cf510d098dc5de628fdcd0f3
digitopoi <hubo85@gmail.com>
2017-10-18 20:21:44 -0500

    Modify discount to take discount size as a parameter in constructor

--- 4_EssentialToolsForMVC/EssentialTools/EssentialTools/Models/Discount.cs
+++ 4_EssentialToolsForMVC/EssentialTools/EssentialTools/Models/Discount.cs
@@ -7,11 +7,16 @@
 
     public class DefaultDiscountHelper : IDiscountHelper
     {
-        public decimal DiscountSize { get; set; }
+        public decimal discountSize;
+
+        public DefaultDiscountHelper(decimal discountParam)
+        {
+            discountSize = discountParam;
+        }
 
         public decimal ApplyDiscount(decimal totalParam)
         {
-            return (totalParam - (DiscountSize / 100m * totalParam));
+            return (totalParam - (discountSize / 100m * totalParam));
         }
     }
 }
